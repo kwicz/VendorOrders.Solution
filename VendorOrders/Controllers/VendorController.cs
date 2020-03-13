a7393ac06faf2ce6f00fc29c0acfd1231816a801
Katy Solovewicz <katy@solovewicz.com>
2020-03-13 10:22:44 -0700

    add Create and Read routes to Vendor Controller

--- VendorOrders/Controllers/VendorController.cs
+++ VendorOrders/Controllers/VendorController.cs
@@ -1,9 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using VendorOrders.Models;
 
 namespace VendorOrders.Controllers
 {
-  public class VendorController : Controllers
+  public class VendorsController : Controllers
   {
-    [HttpGet("/")]
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string address, string phone, string email)
+    {
+      Vendor newVendor = new Vendor(name, address, phone, email);
+      return RedirectToAction("Index");
+    }
   }
 }
